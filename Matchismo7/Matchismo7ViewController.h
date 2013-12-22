75bf92b5690d998066cfff5d81ded56aad9a65a0
Ehab Hamdy <ehabhamdy@Ehabs-Macbook-Pro.local>
2013-12-22 23:26:19 +0200

    Initial Commit

---
+++ Matchismo7/Matchismo7ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Matchismo7ViewController.h
+//  Matchismo7
+//
+//  Created by Ehab Hamdy on 12/22/13.
+//  Copyright (c) 2013 Ehab Hamdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Matchismo7ViewController : UIViewController
+
+@end
---
+++ Matchismo7/Matchismo7ViewController.m
@@ -0,0 +1,29 @@
+//
+//  Matchismo7ViewController.m
+//  Matchismo7
+//
+//  Created by Ehab Hamdy on 12/22/13.
+//  Copyright (c) 2013 Ehab Hamdy. All rights reserved.
+//
+
+#import "Matchismo7ViewController.h"
+
+@interface Matchismo7ViewController ()
+
+@end
+
+@implementation Matchismo7ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
